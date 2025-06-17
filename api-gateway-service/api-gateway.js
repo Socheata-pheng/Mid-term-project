c4a6efaa92d989cc40c967774c72cb82eeb6ae2b
Socheata-pheng <137983043+Socheata-pheng@users.noreply.github.com>
2025-06-17 16:00:18 +0700

    Update api-gateway.js

--- api-gateway-service/api-gateway.js
+++ api-gateway-service/api-gateway.js
@@ -39,23 +39,23 @@ app.use('/student', authToken, authRole('teacher'), (req, res) => {
 });
 
 app.use('/attendance', authToken, authRole('teacher'), (req, res) => {
-    proxy.web(req, res, { target: 'http://3.87.210.76:3005' });
+    proxy.web(req, res, { target: 'http://34.226.148.153:3005' });
 });
 
 app.use('/fee', authToken, authRole('admin'), (req, res) => {
-    proxy.web(req, res, { target: 'http://54.147.54.39:3006' });
+    proxy.web(req, res, { target: 'http://18.233.8.6:3006' });
 });
 
 app.use('/auth', (req, res) => {
-    proxy.web(req, res, { target: 'http://54.210.234.46:3000' });
+    proxy.web(req, res, { target: 'http://44.206.235.183:3000' });
 });
 
 app.use('/course', authToken, authRole('teacher'), (req, res) => {
-    proxy.web(req, res, { target: 'http://44.202.148.138:3002' });
+    proxy.web(req, res, { target: 'http://13.221.169.215:3002' });
 });
 
 app.use('/result', authToken, authRole('teacher'), (req, res) => {
-    proxy.web(req, res, { target: 'http://52.207.98.41:3004' });
+    proxy.web(req, res, { target: 'http://44.204.60.27:3004' });
 });
 
 // ---- START GATEWAY ---- //
